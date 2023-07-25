40c480a281b2afd90327e8ab7ec2e97613e276f3
Amit Kumar <112743124+Lucretia37@users.noreply.github.com>
2023-07-25 12:54:10 +0530

    Update practise.sol

--- practise.sol
+++ practise.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ExampleContract {
+    // A state variable to store the contract owner's address
+    address public contractOwner;
+
+    constructor() {
+        contractOwner = msg.sender;
+    }
+
+    // A view function that returns the contract owner's address
+    function getContractOwner() public view returns (address) {
+        return contractOwner;
+    }
+
+    // A pure function that adds two numbers and returns the result
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+
+    // A payable function that allows users to send Ether to the contract
+    function receiveEther() public payable {
+        // Nothing needs to be done here; this function just accepts Ether
+    }
+}
+
+// A derived contract that inherits from ExampleContract
+contract DerivedContract is ExampleContract {
+    // A new function specific to the derived contract
+    function multiply(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+}
 
