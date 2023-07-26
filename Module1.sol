d0dddbd585db6bcd5638264def3bdae5f4519922
Amit Kumar <112743124+Lucretia37@users.noreply.github.com>
2023-07-26 22:01:17 +0530

    Create Module1.sol

---
+++ Module1.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ErrorHandlingContract {
+    uint256 public balance;
+
+    function deposit(uint256 amount) public {
+        // Use require() to validate the input condition
+        require(amount > 0, "Amount should be greater than zero");
+
+        // Perform the deposit
+        balance += amount;
+    }
+
+    function withdraw(uint256 amount) public {
+        // Use assert() to check for invariants
+        assert(balance >= amount);
+
+        // Use revert() to handle exceptional conditions
+        if (amount <= balance) {
+            balance -= amount;
+        } else {
+            revert("Insufficient balance");
+        }
+    }
+}
